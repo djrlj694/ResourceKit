d880f270b5852a093c2a94192d8a18186dd35d52
djrlj694 <robert.l.jones@synthelytics.com>
2018-12-27 14:47:56 -0500

    Initial project setup.

---
+++ ResourceKit/ResourceKit.h
@@ -0,0 +1,19 @@
+//
+//  ResourceKit.h
+//  ResourceKit
+//
+//  Created by Robert L. Jones on 12/27/18.
+//  Copyright © 2018 Synthelytics LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ResourceKit.
+FOUNDATION_EXPORT double ResourceKitVersionNumber;
+
+//! Project version string for ResourceKit.
+FOUNDATION_EXPORT const unsigned char ResourceKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ResourceKit/PublicHeader.h>
+
+
