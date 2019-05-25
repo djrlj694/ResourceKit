5265ad9b9ea8ed30e06c54492bc94fa8a2caefd9
djrlj694 <robert.l.jones@synthelytics.com>
2019-05-25 10:30:43 -0400

    Update File.swift

--- ResourceKit/Sources/Protocols/File.swift
+++ ResourceKit/Sources/Protocols/File.swift
@@ -3,7 +3,7 @@
 //  ResourceKit
 //
 //  Created by Robert L. Jones on 12/30/18.
-//  Copyright © 2018 Synthelytics LLC. All rights reserved.
+//  Copyright © 2019 djrlj694.dev. All rights reserved.
 //
 
 import Foundation
