680ca7a0f5a2a0d146029c4265efed2d93791adf
djrlj694 <robert.l.jones@synthelytics.com>
2019-05-25 10:30:13 -0400

    Update Data+ResourceKit.swift

--- ResourceKit/Sources/Extensions/Data+ResourceKit.swift
+++ ResourceKit/Sources/Extensions/Data+ResourceKit.swift
@@ -3,7 +3,7 @@
 //  ResourceKit
 //
 //  Created by Robert L. Jones on 1/1/19.
-//  Copyright © 2018 Synthelytics LLC. All rights reserved.
+//  Copyright © 2019 djrlj694.dev. All rights reserved.
 //
 //  REFERENCES:
 //  1. https://www.swiftbysundell.com/posts/type-inference-powered-serialization-in-swift
