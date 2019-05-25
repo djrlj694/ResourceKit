7b9dda6c8ad6a3997bc544c94a1469e4f5cc8df3
djrlj694 <robert.l.jones@synthelytics.com>
2019-05-25 10:30:26 -0400

    Update URL+ResourceKit.swift

--- ResourceKit/Sources/Extensions/URL+ResourceKit.swift
+++ ResourceKit/Sources/Extensions/URL+ResourceKit.swift
@@ -3,7 +3,7 @@
 //  ResourceKit
 //
 //  Created by Robert L. Jones on 12/30/18.
-//  Copyright © 2018 Synthelytics LLC. All rights reserved.
+//  Copyright © 2019 djrlj694.dev. All rights reserved.
 //
 //  REFERENCES:
 //  1. https://github.com/romiphadte/WWDC_2015/blob/master/WWDC_App/AudioController.m
